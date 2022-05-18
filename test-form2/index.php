22a943484aedde7dd9d336411117174d6fed4ae4
Nadin <kallisto.777@mail.ru>
2022-05-18 14:46:13 +0300

    title loc

--- test-form2/index.php
+++ test-form2/index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("test form2 123");
+$APPLICATION->SetTitle("test form2");
 ?><?$APPLICATION->IncludeComponent(
 	"bitrix:form",
 	"",
