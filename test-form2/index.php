f2d88643b2a7c06996c9036609e31c81ffc719c0
Nadin <kallisto.777@mail.ru>
2022-05-18 15:43:37 +0300

    test1

--- test-form2/index.php
+++ test-form2/index.php
@@ -2,7 +2,7 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("test form2");
 ?>
-<p>text123</p>
+<p>text123 654</p>
 <?$APPLICATION->IncludeComponent(
 	"bitrix:form",
 	"",
